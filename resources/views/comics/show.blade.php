ec993c5c1f27d81b5157db6e7a3e221240a3ad5e
pierobramanti <piero.bramanti94@gmail.com>
2024-09-30 22:37:49 +0200

    [UPD] Apply Bootstrap styling to comic details page

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,20 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="bg-dark">
+    <div class="container py-5">
+        <div class="row">
+            <div class="col-md-4 d-flex align-items-center justify-content-center">
+                <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}" class="img-fluid rounded shadow">
+            </div>
+            <div class="col-md-8">
+                <h1 class="display-5 text-white">{{ $comic['title'] }}</h1>
+                <p class="text-muted"><sub class="text-white">{{ $comic['type'] }}</sub></p>
+                <p class="text-white">{{ $comic['description'] }}</p> 
 
-<div class="container">
-    <div class="row">
-        <div class="col-4">
-            <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}" class="img-fluid">
-        </div>
-        <div class="col-8">
-            <h1>{{ $comic['title'] }}</h1>
-            <p><sub>{{ $comic['type'] }}</sub></p>
-            <p>{{ $comic['description'] }}</p> 
-            <h4>{{ $comic['price'] }}</h4> 
-            <p>{{ $comic['sale_date'] }}</p> 
+                <h4 class="fw-bold text-primary">Price: {{ $comic['price'] }}$</h4> 
+                <p class="text-muted">Sale Date: {{ $comic['sale_date'] }}</p>
+            </div>
         </div>
     </div>
 </div>
+
 
 @endsection
