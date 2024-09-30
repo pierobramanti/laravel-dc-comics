5f394164563d7031f2b8d9fe1dc6fa49726812d9
pierobramanti <piero.bramanti94@gmail.com>
2024-09-30 19:36:10 +0200

    [UPD] inserted style and modified template to print images and title

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,35 +1,34 @@
 @extends("layouts.app")
 
-
 @section("content")
 <div class="bg-hero">
 </div>
 <div class="bg-dark">
-<div class="pt-5">
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-
-            <h1>Debug: La view è caricata correttamente</h1>
-
-            @dd($comics)  {{-- Questo mostrerà il contenuto di $comics --}}
-
-                @foreach($comics as $comic)
-                    <div class="comic">
-                        <div class="thumb">
-                            <img src="{{$comic['thumb']}}" alt="{{$comic['title']}}">
-                            <h4 class="text-uppercase">{{$comic['title']}}</h4>
-                        </div>
+    <div class="pt-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="row">
+                        {{-- Ciclo per i fumetti --}}
+                        @foreach($comics as $comic)
+                            <div class="col-md-2 mb-4"> 
+                                <div class="comic text-center">
+                                    <div class="image-container"> {{-- Usa la classe .image-container --}}
+                                        <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}" class="img-fluid">
+                                    </div>
+                                    <h4 class="fs-5 text-white mt-2">{{ $comic['title'] }}</h4>
+                                </div>
+                            </div>
+                        @endforeach
                     </div>
-                @endforeach
-            </div>
-          
-            
-            <div class="col-12">
-                <h1 class="text-white text-center">laravel comics</h1>
+                </div>
+
+                <div class="col-12">
+                    <h1 class="text-white text-center">Laravel Comics</h1>
+                </div>
             </div>
         </div>
     </div>
 </div>
-</div>
 @endsection
+
