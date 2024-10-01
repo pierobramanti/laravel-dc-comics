a15515930245af72526052fe51b761a89e78f23b
pierobramanti <piero.bramanti94@gmail.com>
2024-10-01 18:59:30 +0200

    [UPD] Add edit route and edit view for comics

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -77,9 +77,9 @@ class ComicController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(comic $comic)
     {
-        //
+        return view('comics.edit', compact('comic'));
     }
 
     /**
---
+++ resources/views/comics/edit.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card p-4 shadow-sm bg-light border-0 rounded">
+                <h2 class="text-center mb-4">Aggiungi un nuovo Fumetto</h2>
+                <form action="{{ route('comics.store') }}" method="post">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="title" class="form-label">Titolo</label>
+                        <input type="text" name="title" id="title" class="form-control" placeholder="Inserisci il titolo" value="{{ $comic->title }}">
+                    </div>
+                    <div class="mb-3">
+                        <label for="thumb" class="form-label">Immagine</label>
+                        <input type="text" name="thumb" id="thumb" class="form-control" placeholder="Inserisci URL dell'immagine" value="{{ $comic->thumb }}">
+                    </div>
+                    <div class="mb-3">
+                        <label for="description" class="form-label">Descrizione</label>
+                        <textarea name="description" id="description" class="form-control" placeholder="Inserisci la descrizione"></textarea>
+                    </div>
+                    <div class="mb-3">
+                        <label for="price" class="form-label">Prezzo</label>
+                        <input type="text" name="price" id="price" class="form-control" placeholder="Inserisci il prezzo" value="{{ $comic->price }}">
+                    </div>
+                    <div class="mb-3">
+                        <label for="sale_date" class="form-label">Data di Vendita</label>
+                        <input type="date" name="sale_date" id="sale_date" class="form-control" placeholder="Inserisci la data di vendita" value="{{ $comic->sale_date }}">
+                    </div>
+                    <div class="mb-3">
+                        <label for="type" class="form-label">Tipologia</label>
+                        <input type="text" name="type" id="type" class="form-control" placeholder="Inserisci la tipologia" value="{{ $comic->type }}">
+                    </div>
+                    <div class="mb-3">
+                        <label for="artists" class="form-label">Artisti</label>
+                        <textarea name="artists" id="artists" class="form-control" placeholder="Inserisci gli artisti">{{ $comic->artists }}</textarea>
+                    </div>
+                    <div class="mb-3">
+                        <label for="writers" class="form-label">Scrittori</label>
+                        <textarea name="writers" id="writers" class="form-control" placeholder="Inserisci gli scrittori">{{ $comic->writers }}</textarea>
+                    </div>
+                    <div class="text-center">
+                        <button type="submit" class="btn btn-primary">Salva</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
